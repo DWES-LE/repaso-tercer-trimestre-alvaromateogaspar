7b06c0c25a20a8a5b32b1f3d4108a8be511dde0a
unknown <alvaromateodaw@gmail.com>
2023-04-17 11:10:15 +0200

    creación de sitio web, añadido imagenes, videos y bootstrap

---
+++ mysite/home/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+    video = models.URLField(blank=True)
+    imagen = models.ImageField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('imagen'),
+        FieldPanel('video'),
+
+    ]
+
+

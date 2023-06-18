5f2f281a503e694ea613bcb6535ea75f420fec1e
Álvaro Mateo Gaspar <alvaromateodaw@gmail.com>
2023-06-18 11:32:33 +0200

    Creación volcado de datos

--- acercade/models.py
+++ acercade/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from django.db.models import CharField
 
 class AboutPage(Page):
     template = "acercade/about_page.html"
-    body = RichTextField(blank=True)
+    body = CharField(max_length=8000, null=True, blank=True)
     
     content_panels = Page.content_panels + [
         FieldPanel('body'),
